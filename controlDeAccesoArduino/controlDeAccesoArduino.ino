e67fd80939e812043a7e7416267488063d517a3f
sebastianBrunoUtn <sebastianbruno.utn@gmail.com>
2018-07-04 21:09:49 -0300

    Agrega comportamiento de puerta y nueva tarjeta

--- controlDeAccesoArduino/controlDeAccesoArduino.ino
+++ controlDeAccesoArduino/controlDeAccesoArduino.ino
@@ -2,20 +2,72 @@
 #include <LiquidCrystal.h>
 
 boolean modoMantenimiento = false;
+boolean modoNuevaTarjeta = false;
+boolean puertaAbierta = false;
+const int pinPuerta = 21;
 
 void setup() {
   Serial.begin(9600); //Consola
   Serial2.begin(9600);//ESP
   Serial.println("Funcionando");
+  setUpPuerta();
   setUpLectorTarjeta();
   setUpTeclado();
   setUpDisplay();
 }
 
 void loop() {
-  accesoViaTarjeta();
-  accesoViaClaveDeIngreso();
+  if(modoNuevaTarjeta) {
+    procesarNuevaTarjeta();
+  } else {
+    accesoViaTarjeta();
+    accesoViaClaveDeIngreso();
+    if(leerMensajeDeNuevaTarjeta()) {
+      Serial.println("Recibido pedido de nueva tarjeta");
+      modoNuevaTarjeta = true;
+    }
+  }
+  sonarBuzzer();
 }
+
+/******* Puerta *******/
+const int pinBuzzer = 22;
+unsigned long lastReadPuerta = 0;
+const int debouncePuerta = 200;
+unsigned long puertaUltimaVezAbierta = 0;
+const int timerParaBuzzer = 3000;
+boolean sonandoBuzzer = false;
+
+void setUpPuerta() {
+  pinMode(pinPuerta, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(pinPuerta), cambioEstadoPuerta, CHANGE);
+}
+
+void cambioEstadoPuerta() {
+  if(millis() - lastReadPuerta > debouncePuerta) {
+    Serial.println("CAMBIO PUERTA");
+    if(puertaAbierta) {
+      printEnDisplay("Puerta Cerrada", 0, 0);    
+    } else {
+      printEnDisplay("Puerta Abierta", 0, 0); 
+      puertaUltimaVezAbierta = millis();
+    }
+    
+    lastReadPuerta = millis();
+    puertaAbierta = !puertaAbierta;
+  }
+}
+
+void sonarBuzzer() {
+  if(puertaAbierta && !sonandoBuzzer && (millis() - puertaUltimaVezAbierta) > timerParaBuzzer) {
+    digitalWrite(pinBuzzer, HIGH);
+    sonandoBuzzer = true;
+  } else if(!puertaAbierta) {
+    digitalWrite(pinBuzzer, LOW);
+    sonandoBuzzer = false;
+  }
+}
+/******* *******/
 
 /******* Tarjeta *******/  
 unsigned long lastRead = 0;
@@ -27,19 +79,32 @@ void setUpLectorTarjeta() {
 }
 
 void accesoViaTarjeta() {
-  leerCodigoDeTarjeta();
+  leerCodigoDeTarjeta(false);
   if(codigoLeido != "") {
     procesarRespuestaDeESP();
   }
 }
 
-void leerCodigoDeTarjeta() {
+void procesarNuevaTarjeta() {
+  leerCodigoDeTarjeta(true);
+  if(codigoLeido != "") {
+    modoNuevaTarjeta = false;
+    printEnDisplay("listo!", 0, 0);
+  }
+}
+
+void leerCodigoDeTarjeta(boolean nuevaTarjeta) {
   if (Serial1.available() > 0){
     if(millis() - lastRead > debounce) {
       String codigoTarjeta = Serial1.readStringUntil('\n');
       lastRead = millis();
       Serial.println(codigoTarjeta);
-      Serial2.println("LEE:"+codigoTarjeta);
+      if(nuevaTarjeta) {
+        Serial.println("Mando TAR:" +codigoTarjeta);
+        Serial2.println("TAR:"+codigoTarjeta);
+      } else {
+        Serial2.println("LEE:"+codigoTarjeta);
+      }
       codigoLeido = codigoTarjeta;
     } else {
       while(Serial1.available()){
@@ -85,8 +150,22 @@ void procesarClaveIngresada(String clave) {
 /******* *******/
 
 /******* ESP *******/
+boolean leerMensajeDeNuevaTarjeta() {
+  if (Serial2.available() > 0){
+    String lectura = Serial2.readStringUntil('\n');
+    lectura[sizeof(lectura)-1]=0;
+    
+    Serial.println("Recibido: " + lectura);
+    if(lectura.equals("NEW:a")) {     
+      printEnDisplay("Acerce su tarjeta", 0, 0);
+      Serial.println("Pedido");
+    }
+    return lectura=="NEW:a";
+  }
+  return false;
+}
+
 void procesarRespuestaDeESP() {
-  Serial.println("Procesando");
   if (Serial2.available() > 0){
     String lectura = Serial2.readStringUntil('\n');
     lectura[sizeof(lectura)-1]=0;
